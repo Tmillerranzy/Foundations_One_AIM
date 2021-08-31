8fc5cfaac5ee3c843a8fc731e66cac9d37ea6677
Totiaunah <totiaunahranzy@gmail.com>
2021-08-31 14:57:53 -0500

    update transitions 1

---
+++ foundations-of-web-development/transitions 1/script.js
@@ -0,0 +1,17 @@
+$(function() {
+    $("#move").click(function() {
+        $("#block1").toggleClass("move");
+    });
+
+    $("#size").click(function() {
+        $("#block1").toggleClass("size");
+    });
+
+    $("#opacity").click(function() {
+        $("#block1").toggleClass("opacity");
+    });
+
+    $("#color").click(function() {
+        $("#block1").toggleClass("color");
+    });
+});
